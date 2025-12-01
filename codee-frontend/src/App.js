1b87b81902a7e38f1e81bdf37a50e291ad9b26d0
Bilal Asif Burney <bilalburney14@gmail.com>
2025-12-01 20:39:45 +0500

    add frontend setup

---
+++ codee-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CodingAssistant from './components/CodingAssistant';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <CodingAssistant />
+    </div>
+  );
+}
+
+export default App;
